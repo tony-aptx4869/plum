949e7db3451046830df6101af211ded68b354ac7
Tony Chang <tonychang7869@gmail.com>
2022-11-11 15:01:41 +0800

    Update tony-packages, and add a windows version

---
+++ tony-packages.bat
@@ -0,0 +1,17 @@
+set package_list=%package_list%^
+ prelude^
+ essay^
+ luna-pinyin^
+ tony-aptx4869/rime-combo-pinyin^
+ double-pinyin^
+ terra-pinyin^
+ bopomofo^
+ cangjie^
+ stroke^
+ cantonese^
+ jyutping^
+ emoji^
+ ipa^
+ middle-chinese^
+ scj^
+ 
